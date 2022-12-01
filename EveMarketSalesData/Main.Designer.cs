ae6bfdaec4f1efdad65839f80ebea8be86b3584d
Sir丶雨轩 <1718018032@qq.com>
2022-12-01 16:26:59 +0800

    添加项目文件。

---
+++ EveMarketSalesData/Main.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace EveMarketSalesData
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.treeView_type = new System.Windows.Forms.TreeView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lab_pro = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lab_type = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lab_region = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.treeView_type);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(252, 409);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "分类区";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 27);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 12);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "地区";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(41, 23);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(203, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // treeView_type
+            // 
+            this.treeView_type.Location = new System.Drawing.Point(6, 49);
+            this.treeView_type.Name = "treeView_type";
+            this.treeView_type.Size = new System.Drawing.Size(238, 354);
+            this.treeView_type.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.listView1);
+            this.groupBox2.Location = new System.Drawing.Point(281, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(731, 402);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "结果区";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(630, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "搜索";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "商品名称";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(6, 61);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(719, 323);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "序号";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "商品名称";
+            this.columnHeader2.Width = 200;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "平均日销量";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "平均成交价格";
+            this.columnHeader4.Width = 100;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "吉他价格";
+            this.columnHeader5.Width = 100;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "最后成交日期";
+            this.columnHeader6.Width = 120;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lab_region);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Controls.Add(this.lab_type);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.linkLabel1);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.progressBar1);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.lab_pro);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Location = new System.Drawing.Point(18, 430);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(994, 100);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "执行区";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(350, 76);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(137, 12);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "混沌仲裁者欢迎您的加入";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(881, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 47);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "开始执行";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(282, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "尚未开始";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(227, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "耗时详情:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(81, 71);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(252, 20);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "当前进度:";
+            // 
+            // lab_pro
+            // 
+            this.lab_pro.AutoSize = true;
+            this.lab_pro.ForeColor = System.Drawing.Color.Red;
+            this.lab_pro.Location = new System.Drawing.Point(282, 26);
+            this.lab_pro.Name = "lab_pro";
+            this.lab_pro.Size = new System.Drawing.Size(53, 12);
+            this.lab_pro.TabIndex = 1;
+            this.lab_pro.Text = "尚未开始";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(227, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "正在处理:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(18, 26);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 12);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "所选分类:";
+            // 
+            // lab_type
+            // 
+            this.lab_type.AutoSize = true;
+            this.lab_type.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.lab_type.Location = new System.Drawing.Point(81, 26);
+            this.lab_type.Name = "lab_type";
+            this.lab_type.Size = new System.Drawing.Size(65, 12);
+            this.lab_type.TabIndex = 9;
+            this.lab_type.Text = "蓝图和反应";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(18, 51);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(59, 12);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "所选分类:";
+            // 
+            // lab_region
+            // 
+            this.lab_region.AutoSize = true;
+            this.lab_region.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.lab_region.Location = new System.Drawing.Point(81, 49);
+            this.lab_region.Name = "lab_region";
+            this.lab_region.Size = new System.Drawing.Size(41, 12);
+            this.lab_region.TabIndex = 11;
+            this.lab_region.Text = "德克里";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 542);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EVE市场销售数据统计 By：Sir丶雨轩";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TreeView treeView_type;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lab_pro;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label lab_region;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lab_type;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ EveMarketSalesData/Main.cs
@@ -0,0 +1,117 @@
+﻿using DataModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EveMarketSalesData
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        Dictionary<string,long> regionPairs = new Dictionary<string,long>();
+        private void Main_Load(object sender, EventArgs e)
+        {
+            MarketDataDB db = new MarketDataDB();
+
+            var regions  = from table in db.EveRegions select table;
+            foreach (var region in regions)
+            {
+                regionPairs.Add(region.Name, region.Id);
+                comboBox1.Items.Add(region.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+            var result = from table in db.EveMarketGroups.Where(item => item.ParentGroupId == 0) select table;
+            foreach (var item in result)
+            {
+                TreeNode topNode = new TreeNode(item.NameZh);
+                topNode.Tag = item.Id;
+                treeView_type.Nodes.Add(topNode);
+                FillChildren(topNode.Nodes, item.Id);
+            }
+        }
+
+        private void FillChildren(TreeNodeCollection node,long? id) 
+        {
+            MarketDataDB db = new MarketDataDB();
+            var result = from table in db.EveMarketGroups.Where(item => item.ParentGroupId == id)
+                         select table;
+            foreach (var item in result)
+            {
+                TreeNode topNode = new TreeNode(item.NameZh);
+                topNode.Tag = item.Id;
+                node.Add(topNode);
+                FillChildren(topNode.Nodes, item.Id);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (treeView_type.SelectedNode == null) 
+            {
+                MessageBox.Show("请先选择分类", "警告",MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // 开始获取所选分类下的所有分类
+
+            List<long?> groupIds = new List<long?>();
+            MarketDataDB db = new MarketDataDB();
+            var result = from table in db.EveMarketGroups.Where(item => item.ParentGroupId == Convert.ToInt64(treeView_type.SelectedNode.Tag)) select table;
+            while (result.Count() >0) 
+            {
+                List<long?> ids = new List<long?>();
+                foreach (var item in result) 
+                {
+                    ids.Add(item.Id);
+                    groupIds.Add(item.Id);
+                }
+                MarketDataDB db1 = new MarketDataDB();
+                result = from table1 in db1.EveMarketGroups.Where(item => ids.Contains(item.ParentGroupId)) select table1;
+            }
+
+            var typeGroups = from table in db.EveTypeGroups.Where(item => groupIds.Contains(item.GroupId)) select table;
+            List<long?> typeIds =   new List<long?>();
+            foreach (var typeGroup in typeGroups) 
+            {
+               typeIds.Add(typeGroup.TypeId);
+            }
+
+            Dictionary<long?,string> idNamePairs = new Dictionary<long?,string>();
+            var idName = from table in db.EveItemNames.Where(item=>item.Type == 8) select table;
+            foreach (var idNamePair in idName) 
+            {
+                if (!idNamePairs.ContainsKey(idNamePair.ItemId)) 
+                {
+                    idNamePairs.Add(idNamePair.ItemId, idNamePair.ZhName);
+                }
+                
+            }
+            lab_pro.ForeColor = Color.LightSeaGreen;
+            foreach (var typeId in typeIds) 
+            {
+                if (idNamePairs.ContainsKey(typeId))
+                {
+                    lab_pro.Text = idNamePairs[typeId];
+                }
+                else 
+                {
+                    lab_pro.Text = "未知名称";
+                }
+                Thread.Sleep(200);
+
+            }
+
+
+        }
+    }
+}
